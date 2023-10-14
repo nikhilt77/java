8c5236c770091e9c9ff77677d5d2c5b9382b641a
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-10-14 10:16:25 +0530

    Create Multiplestring.java
    Enter a limit to print  a pattern like:
    1 Hello world
    2 Hello
    3 World
    .....
    n String

---
+++ Multiplestring.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner P=new Scanner(System.in);
+       int a=1;
+       while(P.hasNext()){
+           String str=P.nextLine();
+           System.out.println(a+" "+str);
+           a++;
+        
+       }
+    }
+}
