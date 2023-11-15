3131bd090db4c5064c8b3ea8a0fcf4586f384466
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-11-15 21:46:51 +0530

    Update Substring.java

--- Substring.java
+++ Substring.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Solution {
-
+//Creating main method
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
