780033b41ba05dd475f48ee44dbfdb88a7f41d46
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-12-21 00:08:54 +0530

    Create UpperLower.java

---
+++ UpperLower.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class UpperLower {
+
+	public static void main(String[] args) {
+		Scanner P=new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String str=P.nextLine();
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
+
+	}
+
+}
