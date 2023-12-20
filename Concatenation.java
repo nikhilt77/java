5f0e2b3dcb05edb281147283c65fb1d15f9b20ba
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2023-12-21 00:07:51 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Concatenation {
+
+	public static void main(String[] args) {
+		Scanner p=new Scanner(System.in);
+		System.out.print("Enter String 1: ");
+		String s1=p.nextLine();
+		System.out.print("Enter String 2: ");
+		String s2=p.nextLine();
+		System.out.println(s1+""+s2);
+	}
+
+}
