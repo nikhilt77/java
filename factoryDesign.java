6ccc1bb44245292db34a3b1e81c02528bcb1a97a
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2025-01-08 23:49:46 +0530

    Added equalsIgnore

--- factoryDesign.java
+++ factoryDesign.java
@@ -7,15 +7,19 @@ interface ATM {
     void display();
 }
 
-class SBI implements ATM{
-    private int balance=0;
+class SBI implements ATM {
+
+    private int balance = 0;
+
     public void getBalance() {
         System.out.println("Your balance is: " + balance);
     }
+
     public void deposit(int amount) {
         balance += amount;
         System.out.println("Amount deposited successfully");
     }
+
     public void withdraw(int amount) {
         if (amount > balance) {
             System.out.println("Insufficient balance");
@@ -24,20 +28,25 @@ class SBI implements ATM{
             System.out.println("Amount withdrawn successfully");
         }
     }
+
     public void display() {
         System.out.println("Welcome to SBI Bank");
     }
 }
 
-class PNB implements ATM{
-    private int balance=0;
+class PNB implements ATM {
+
+    private int balance = 0;
+
     public void getBalance() {
         System.out.println("Your balance is: " + balance);
     }
+
     public void deposit(int amount) {
         balance += amount;
         System.out.println("Amount deposited successfully");
     }
+
     public void withdraw(int amount) {
         if (amount > balance) {
             System.out.println("Insufficient balance");
@@ -46,36 +55,38 @@ class PNB implements ATM{
             System.out.println("Amount withdrawn successfully");
         }
     }
+
     public void display() {
         System.out.println("Welcome to PNB Bank");
     }
 }
-class Bank{
-    public ATM getBank(String bank){
-        if(bank.equals("SBI")){
+
+class Bank {
+
+    public ATM getBank(String bank) {
+        if (bank.equalsIgnoreCase("SBI")) {
             return new SBI();
-        }
-        else if(bank.equals("PNB")){
+        } else if (bank.equalsIgnoreCase("PNB")) {
             return new PNB();
         }
         return null;
     }
 }
-public class factoryDesign{
-    public static void main(String args[]){
 
-        Scanner P=new Scanner(System.in);
-        Bank obj=new Bank();
+public class factoryDesign {
+
+    public static void main(String args[]) {
+        Scanner P = new Scanner(System.in);
+        Bank obj = new Bank();
         System.out.println("Enter the bank name: ");
-        String bank=P.nextLine();
-        bank.toUpperCase();
-        ATM bank1=(ATM)obj.getBank(bank);
+        String bank = P.nextLine();
+        ATM bank1 = (ATM) obj.getBank(bank);
         bank1.display();
         System.out.print("Enter Deposit:");
-        int deposit=P.nextInt();
+        int deposit = P.nextInt();
         bank1.deposit(deposit);
         System.out.print("Enter Withdraw:");
-        int withdraw=P.nextInt();
+        int withdraw = P.nextInt();
         bank1.withdraw(withdraw);
         bank1.getBalance();
     }
